a988fc3ad70cb4f4e7e96b6198272440f5453f26
Richard Clamp <richardc@unixbeard.net>
2014-05-03 11:13:25 +0100

    use boost program_options

--- src/broker_main.cpp
+++ src/broker_main.cpp
@@ -1,9 +1,27 @@
 #include "broker.h"
 #include "logger.h"
+#include <boost/program_options.hpp>
 
-int main() {
+namespace po = boost::program_options;
+
+int main(int argc, char *argv[]) {
+    po::options_description desc("Allowed options");
+    desc.add_options()
+        ("help", "display help")
+        ("bind", po::value<std::string>()->default_value("tcp://*:61616"), "address to bind")
+        ("curve-private-key", po::value<std::string>()->required(), "private key")
+    ;
+
+    po::variables_map vm;
+    po::store(parse_command_line(argc, argv, desc), vm);
+
+    if (vm.count("help")) {
+        std::cout << desc;
+        return 0;
+    }
+
     Logger logger;
-    Broker broker("tcp://*:61616", "middleware.private");
+    Broker broker(vm["bind"].as<std::string>(), vm["curve-private-key"].as<std::string>());
     broker.main_loop();
     return 0;
 }
