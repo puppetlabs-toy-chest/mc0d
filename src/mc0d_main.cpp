c05d9c6192d666b2f5bf9de9055c3597c3ac2156
Richard Clamp <richardc@unixbeard.net>
2014-05-12 17:35:22 +0100

    split out bind address and port
    We always want the endpoint to be tcp for the stream semantics, so formulate
    the endpoint from the equivalent of "tcp://%s:%d"

--- src/mc0d_main.cpp
+++ src/mc0d_main.cpp
@@ -1,6 +1,7 @@
 #include "broker.h"
 #include "logger.h"
 #include <boost/program_options.hpp>
+#include <string>
 
 namespace po = boost::program_options;
 
@@ -8,7 +9,8 @@ int main(int argc, char *argv[]) {
     po::options_description desc("Allowed options");
     desc.add_options()
         ("help", "display help")
-        ("bind", po::value<std::string>()->default_value("tcp://*:61616"), "address to bind")
+        ("bind", po::value<std::string>()->default_value("*"), "address to bind")
+        ("port", po::value<int>()->default_value(61616), "port to bind")
         ("curve-private-key", po::value<std::string>()->required(), "private key")
         ("logger-config", po::value<std::string>()->default_value(""), "log4cxx properties file")
     ;
@@ -22,7 +24,8 @@ int main(int argc, char *argv[]) {
     }
 
     mc0::Logger logger(vm["logger-config"].as<std::string>());
-    mc0::Broker broker(vm["bind"].as<std::string>(), vm["curve-private-key"].as<std::string>());
+    std::string endpoint("tcp://" + vm["bind"].as<std::string>() + ":" + std::to_string(vm["port"].as<int>()));
+    mc0::Broker broker(endpoint, vm["curve-private-key"].as<std::string>());
     broker.main_loop();
     return 0;
 }
